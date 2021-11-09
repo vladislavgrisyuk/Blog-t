dae6ada2700e2d367effaebc8ce7f0538ba390fa
v.grysiuk <v.grysiuk@temabit.com>
2021-11-09 19:22:01 +0200

    FUCK

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const app = express()
-//tesdt commit v.2
+//tesdt commit v.3
 app.listen(3000, () => {
     console.log('Server is listenting on port 3000!')
 })
